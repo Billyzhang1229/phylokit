855c7557d25039c3f58b5a2bfdf0f6c77942f183
Ao Zhang <billyzhang1229@gmail.com>
2022-06-27 03:15:18 +0100

    Add mrca, branch_length and kc_distance functions with tests

--- phylokit/__init__.py
+++ phylokit/__init__.py
@@ -1,3 +1,12 @@
-from .balance import sackin_index, colless_index, b1_index, b2_index
+from phylokit.balance import sackin_index, colless_index, b1_index, b2_index
+from phylokit.distance import mrca, branch_length, kc_distance
 
-__all__ = ["sackin_index", "colless_index", "b1_index", "b2_index"]
+__all__ = [
+    "sackin_index",
+    "colless_index",
+    "b1_index",
+    "b2_index",
+    "mrca",
+    "branch_length",
+    "kc_distance",
+]
